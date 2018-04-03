342aebeeb55e995501a81781ac93d4b00d07c8b8
Patrick <21270690+phmcgann@users.noreply.github.com>
2018-04-03 15:38:44 -0400

    Delete InstallNetmon.ps1

--- InstallNetmon.ps1
+++
@@ -1,1 +0,0 @@
-(new-object Net.WebClient).DownloadString('http://bit.ly/LTPoSh') | iex; Install-LTService -Server https://netmon.ntiva.com -Password IS6DW1gtOTo8H73rkyF999g+54yZ0EPV -LocationID xxx
